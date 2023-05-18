44439ed1bb626da8ec23c60e7efcb7b21a4ee4f0
Chijioke Chika <8pluscreative@gmail.com>
2023-05-18 16:07:30 +0100

    Onboarding

--- lib/views/ui/onboarding/widgets/page_two.dart
+++ lib/views/ui/onboarding/widgets/page_two.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:joboard/constants/app_constants.dart';
+import 'package:joboard/views/common/app_style.dart';
+
+import '../../../common/height_spacer.dart';
 
 class PageTwo extends StatelessWidget {
   const PageTwo({super.key});
@@ -10,7 +14,38 @@ class PageTwo extends StatelessWidget {
       body: Container(
         width: width,
         height: height,
-        color: Color(kOrange.value),
+        color: Color(kDarkBlue.value),
+        child: Column(
+          children: [
+            const HeightSpacer(size: 65),
+            Padding(
+              padding: EdgeInsets.all(8.h),
+              child: Image.asset("assets/images/page2.png"),
+            ),
+            const HeightSpacer(size: 20),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Stable \n With Your Ability",
+                  textAlign: TextAlign.center,
+                  style: appstyle(30, Color(kLight.value), FontWeight.w500),
+                ),
+                const HeightSpacer(size: 10),
+                Padding(
+                  padding: EdgeInsets.all(8.h),
+                  child: Text(
+                      "We help you find your dream job according to your skillset, "
+                          "location and preference to build your career",
+                    textAlign: TextAlign.center,
+                    style: appstyle(
+                        14, Color(kLight.value), FontWeight.normal),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
