345d260354229cc205bf691b9c365da5569389bf
Chijioke Chika <8pluscreative@gmail.com>
2023-05-18 16:48:02 +0100

    Onboarding

--- lib/views/common/custom_outline_btn.dart
+++ lib/views/common/custom_outline_btn.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:joboard/views/common/exports.dart';
 
 class CustomOutlineBtn extends StatelessWidget {
-  const CustomOutlineBtn({super.key});
+  const CustomOutlineBtn(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.text,
+      required this.onTap,
+      required this.color,
+      this.color2});
+
+  final double? width;
+  final double? height;
+  final String text;
+  final void Function()? onTap;
+  final Color color;
+  final Color? color2;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+            color: color2, border: Border.all(width: 1, color: color)),
+        child: Center(
+          child: ReusableText(
+              text: text, style: appstyle(16, color, FontWeight.w600)),
+        ),
+      ),
+    );
   }
 }
--- lib/views/ui/onboarding/widgets/page_three.dart
+++ lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:joboard/constants/app_constants.dart';
+import 'package:joboard/views/common/app_style.dart';
+import 'package:joboard/views/common/custom_outline_btn.dart';
+import 'package:joboard/views/common/height_spacer.dart';
+
+import '../../../common/reusable_text.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({super.key});
@@ -10,7 +16,39 @@ class PageThree extends StatelessWidget {
       body: Container(
         width: width,
         height: height,
-        color: Color(kDarkBlue.value),
+        color: Color(kLightBlue.value),
+        child: Column(
+          children: [
+            Image.asset("assets/images/page3.png"),
+            const HeightSpacer(size: 20),
+            ReusableText(
+              text: "Welcome To Joboard",
+              style: appstyle(30, Color(kLight.value), FontWeight.w600),
+            ),
+            const HeightSpacer(size: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: Text(
+                "We help you find your dream job according to your skillset, "
+                "location and preference to build your career",
+                textAlign: TextAlign.center,
+                style: appstyle(14, Color(kLight.value), FontWeight.normal),
+              ),
+            ),
+            const HeightSpacer(size: 20),
+            Row(
+              children: [
+                CustomOutlineBtn(
+                  onTap: () {},
+                  text: "Login",
+                  width: width * 0.4,
+                  height: height * 0.06,
+                  color: Color(kLight.value),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
