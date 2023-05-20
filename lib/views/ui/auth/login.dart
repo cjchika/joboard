9ecf3282dbebad440c2633ab611b5f7e313a083e
Chijioke Chika <8pluscreative@gmail.com>
2023-05-20 06:01:20 +0100

    Login

--- lib/views/ui/auth/login.dart
+++ lib/views/ui/auth/login.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/get_core.dart';
+import 'package:joboard/controllers/exports.dart';
 import 'package:joboard/views/common/custom_btn.dart';
 import 'package:joboard/views/common/custom_textfield.dart';
 import 'package:joboard/views/common/exports.dart';
 import 'package:joboard/views/common/height_spacer.dart';
+import 'package:joboard/views/ui/auth/signup.dart';
+import 'package:provider/provider.dart';
 
 import '../../../constants/app_constants.dart';
 import '../../common/app_bar.dart';
@@ -30,79 +33,89 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: CustomAppBar(
-            text: "Login",
-            child: GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: const Icon(Icons.arrow_back),
+    return Consumer<LoginNotifier>(builder: (context, loginNotifier, child) {
+      return Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: CustomAppBar(
+              text: "Login",
+              child: GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: const Icon(Icons.arrow_back),
+              ),
             ),
           ),
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: ListView(padding: EdgeInsets.zero, children: [
-            const HeightSpacer(size: 50),
-            ReusableText(
-              text: "Welcome Back!",
-              style: appstyle(30, Color(kDark.value), FontWeight.w600),
-            ),
-            const HeightSpacer(size: 10),
-            ReusableText(
-              text: "Fill the details to login to your account.",
-              style: appstyle(15, Color(kDarkBlue.value), FontWeight.w600),
-            ),
-            const HeightSpacer(size: 50),
-            CustomTextField(
-              controller: email,
-              keyboardType: TextInputType.emailAddress,
-              hintText: "Email",
-              validator: (email) {
-                if (email!.isEmpty || !email.contains("@")) {
-                  return "Please enter a valid email";
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const HeightSpacer(size: 20),
-            CustomTextField(
-              controller: email,
-              keyboardType: TextInputType.emailAddress,
-              hintText: "Password",
-              validator: (password) {
-                if (password!.isEmpty || password.length < 7) {
-                  return "Please enter a valid password";
-                } else {
-                  return null;
-                }
-              },
-              suffixIcon: GestureDetector(
-                child: Icon(
-                  Icons.visibility,
-                  color: Color(kDark.value),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: ListView(padding: EdgeInsets.zero, children: [
+              const HeightSpacer(size: 50),
+              ReusableText(
+                text: "Welcome Back!",
+                style: appstyle(30, Color(kDark.value), FontWeight.w600),
+              ),
+              const HeightSpacer(size: 10),
+              ReusableText(
+                text: "Fill the details to login to your account.",
+                style: appstyle(15, Color(kDarkBlue.value), FontWeight.w600),
+              ),
+              const HeightSpacer(size: 50),
+              CustomTextField(
+                controller: email,
+                keyboardType: TextInputType.emailAddress,
+                hintText: "Email",
+                validator: (email) {
+                  if (email!.isEmpty || !email.contains("@")) {
+                    return "Please enter a valid email";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              const HeightSpacer(size: 20),
+              CustomTextField(
+                controller: password,
+                keyboardType: TextInputType.emailAddress,
+                hintText: "Password",
+                obscureText: loginNotifier.obscureText,
+                validator: (password) {
+                  if (password!.isEmpty || password.length < 7) {
+                    return "Please enter a valid password";
+                  } else {
+                    return null;
+                  }
+                },
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    loginNotifier.obscureText = !loginNotifier.obscureText;
+                  },
+                  child: Icon(
+                    loginNotifier.obscureText
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Color(kDark.value),
+                  ),
                 ),
               ),
-            ),
-            const HeightSpacer(size: 10),
-            Align(
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: null,
-                child: ReusableText(
-                  text: "Register",
-                  style: appstyle(14, Color(kDark.value), FontWeight.w500),
+              const HeightSpacer(size: 10),
+              Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    Get.to(() => const RegistrationPage());
+                  },
+                  child: ReusableText(
+                    text: "Register",
+                    style: appstyle(14, Color(kDark.value), FontWeight.w500),
+                  ),
                 ),
               ),
-            ),
-            const HeightSpacer(size: 50),
-            CustomButton(onTap: () {}, text: "Login"),
-          ]),
-        ));
+              const HeightSpacer(size: 50),
+              CustomButton(onTap: () {}, text: "Login"),
+            ]),
+          ));
+    });
     ;
   }
 }
