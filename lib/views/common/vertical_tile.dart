a3837df901d104d86eb8df432344fb970e9b71b3
Chijioke Chika <8pluscreative@gmail.com>
2023-05-21 06:22:59 +0100

    HomeScreen

--- lib/views/common/vertical_tile.dart
+++ lib/views/common/vertical_tile.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:joboard/constants/app_constants.dart';
 
 class VerticalTile extends StatelessWidget {
-  const VerticalTile({super.key});
+  const VerticalTile({super.key, this.onTap});
+
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+        height: height*0.15,
+        width: width,
+        color: Color(kLightGrey.value),
+      ),
+    );
   }
 }
+
+
+
+
+
+
