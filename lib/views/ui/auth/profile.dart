c8a4bd3a7cad2de1091962da2b9d457d14e27fcf
Chijioke Chika <8pluscreative@gmail.com>
2023-05-22 18:08:53 +0100

    Profile

--- lib/views/ui/auth/profile.dart
+++ lib/views/ui/auth/profile.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:joboard/constants/app_constants.dart';
 import 'package:joboard/views/common/app_style.dart';
+import 'package:joboard/views/common/height_spacer.dart';
 import 'package:joboard/views/common/reusable_text.dart';
 import 'package:joboard/views/common/width_spacer.dart';
 
@@ -30,40 +33,95 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          Container(
-            width: width,
-            height: height * 0.12,
-            color: Color(kLight.value),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  child: CachedNetworkImage(
-                    width: 80.w,
-                    height: 100.h,
-                    imageUrl:
-                        "https://gcavocats.ca/wp-content/uploads/2018/09/man-avatar-icon-flat-vector-19152370-1.jpg",
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              width: width,
+              height: height * 0.12,
+              color: Color(kLight.value),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        child: CachedNetworkImage(
+                          width: 60.w,
+                          height: 100.h,
+                          imageUrl:
+                              "https://gcavocats.ca/wp-content/uploads/2018/09/man-avatar-icon-flat-vector-19152370-1.jpg",
+                        ),
+                      ),
+                      const WidthSpacer(width: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ReusableText(
+                            text: "Chijioke Chika",
+                            style: appstyle(
+                                20, Color(kDark.value), FontWeight.w600),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.location_circle_fill,
+                                color: Color(kDarkGrey.value),
+                              ),
+                              const WidthSpacer(width: 5),
+                              ReusableText(
+                                text: "Abuja Nigeria",
+                                style: appstyle(16, Color(kDarkGrey.value),
+                                    FontWeight.w600),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                ),
-                const WidthSpacer(width: 20),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ReusableText(
-                      text: "John Doe",
-                      style: appstyle(20, Color(kDark.value), FontWeight.w600),
-                    )
-                  ],
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(
+                      Feather.edit,
+                      size: 18,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const HeightSpacer(size: 20),
+            Stack(
+              children: [
+                Container(
+                  width: width,
+                  height: height * 0.12,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 12.w),
+                        width: 60.w,
+                        height: 70.h,
+                        color: Color(kLight.value),
+                        child: const Icon(
+                          FontAwesome5Regular.file_pdf,
+                          color: Colors.red,
+                          size: 40,
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
