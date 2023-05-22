dac0d35bd847c0c7eb5d4d372278241a803d1a89
Chijioke Chika <8pluscreative@gmail.com>
2023-05-22 20:30:55 +0100

    Device Mgt Screen

--- lib/views/ui/device_mgt/devices_info.dart
+++ lib/views/ui/device_mgt/devices_info.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:joboard/controllers/exports.dart';
 import 'package:joboard/views/common/app_style.dart';
+import 'package:joboard/views/common/exports.dart';
 import 'package:joboard/views/common/height_spacer.dart';
 import 'package:joboard/views/ui/device_mgt/widgets/device_info.dart';
+import 'package:joboard/views/ui/onboarding/onboarding_screen.dart';
+import 'package:provider/provider.dart';
 
 import '../../../constants/app_constants.dart';
 import '../../common/app_bar.dart';
@@ -13,6 +19,12 @@ class DeviceManagement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var zoomNotifier = Provider.of<ZoomNotifier>(context);
+    var onBoardNotifier = Provider.of<OnBoardNotifier>(context);
+
+    String date = DateTime.now().toString();
+    String loginDate = date.substring(0, 11);
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.h),
@@ -38,10 +50,43 @@ class DeviceManagement extends StatelessWidget {
                     style: appstyle(16, Color(kDark.value), FontWeight.normal),
                   ),
                   const HeightSpacer(size: 50),
-                  const DeviceInfo(),
+                  DeviceInfo(
+                    location: "Abuja NG",
+                    device: "MacBook M2",
+                    platform: "Web Client",
+                    date: loginDate,
+                    ipAddress: "16.0.13.000",
+                  ),
+                  const HeightSpacer(size: 45),
+                  DeviceInfo(
+                    location: "Abia NG",
+                    device: "iPhone 14 Pro Max",
+                    platform: "Mobile",
+                    date: loginDate,
+                    ipAddress: "17.0.12.000",
+                  ),
                 ],
               ),
-            )
+            ),
+           Consumer<LoginNotifier>(builder: (context, loginNotifier, child) {
+             return  Padding(
+               padding: EdgeInsets.all(8.0.h),
+               child: GestureDetector(
+                 onTap: () {
+                   zoomNotifier.currentIndex = 0;
+                   onBoardNotifier.isLastPage = false;
+                   Get.to(() => const OnBoardingScreen());
+                 },
+                 child: Align(
+                   alignment: Alignment.bottomCenter,
+                   child: ReusableText(
+                     text: "Sign Out On All Devices",
+                     style: appstyle(16, Color(kOrange.value), FontWeight.w600),
+                   ),
+                 ),
+               ),
+             );
+           })
           ],
         ),
       ),
--- lib/views/ui/device_mgt/widgets/device_info.dart
+++ lib/views/ui/device_mgt/widgets/device_info.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:joboard/views/common/custom_outline_btn.dart';
+import 'package:joboard/views/common/exports.dart';
+import 'package:joboard/views/common/height_spacer.dart';
 
 class DeviceInfo extends StatelessWidget {
   const DeviceInfo({
@@ -18,11 +21,44 @@ class DeviceInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ReusableText(
+          text: platform,
+          style: appstyle(22, Color(kDark.value), FontWeight.bold),
+        ),
+        ReusableText(
+          text: device,
+          style: appstyle(22, Color(kDark.value), FontWeight.bold),
+        ),
+        const HeightSpacer(size: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ReusableText(
+                  text: date,
+                  style: appstyle(16, Color(kDarkGrey.value), FontWeight.w400),
+                ),
+                ReusableText(
+                  text: ipAddress,
+                  style: appstyle(16, Color(kDarkGrey.value), FontWeight.w400),
+                ),
+              ],
+            ),
+            CustomOutlineBtn(
+              width: width*0.3,
+              height: height*0.05,
+              text: "Sign Out",
+              onTap: () {},
+              color: Color(kOrange.value),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
-
-
-
-
-
