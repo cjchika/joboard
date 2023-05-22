63c301d7760c121ed261d3b99fffd6593fa5c207
Chijioke Chika <8pluscreative@gmail.com>
2023-05-22 21:19:59 +0100

    Search Screen

--- lib/views/ui/search/searchpage.dart
+++ lib/views/ui/search/searchpage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:joboard/views/ui/search/widgets/custom_field.dart';
+
+import '../../../constants/app_constants.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,8 +12,23 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController search = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(kOrange.value),
+        iconTheme: IconThemeData(color: Color(kLight.value)),
+        title: CustomField(
+          hintText: "Search for a role",
+          controller: search,
+          suffixIcon: GestureDetector(onTap: () {
+            setState(() {});
+          }, child: const Icon(AntDesign.search1),),
+        ),
+        elevation: 0,
+      ),
+    );
   }
 }
