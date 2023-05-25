6840abc7cfd7e13a5c27e89f4a952328db23b84b
Chijioke Chika <8pluscreative@gmail.com>
2023-05-25 21:12:05 +0100

    Api Integration

--- lib/services/helpers/auth_helper.dart
+++ lib/services/helpers/auth_helper.dart
@@ -1,6 +1,32 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as https;
+// import 'package:joboard/models/request/auth/login_model.dart';
+// import 'package:joboard/services/config.dart';
 
 class AuthHelper {
   static var client = https.Client();
 
+  // static Future<bool> login(LoginModel model) async {
+  //   Map<String, String> requestHeaders = {
+  //     'Content-Type': "application/json"
+  //   };
+  //
+  //   var url = Uri.https(Config.apiUrl, Config.loginUrl);
+  //
+  //   var response = await client.post(url, headers: , body: jsonEncode(model), )
+  // }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
