cce4e71ed0fdd83dd5921ffa4a4536011ff7f0f7
Chijioke Chika <8pluscreative@gmail.com>
2023-05-27 22:28:48 +0100

    API Auth Integration

--- lib/services/helpers/auth_helper.dart
+++ lib/services/helpers/auth_helper.dart
@@ -1,23 +1,32 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as https;
-// import 'package:joboard/models/request/auth/login_model.dart';
-// import 'package:joboard/services/config.dart';
+import 'package:joboard/models/request/auth/login_model.dart';
+import 'package:joboard/services/config.dart';
 
 class AuthHelper {
   static var client = https.Client();
 
-  // static Future<bool> login(LoginModel model) async {
-  //   Map<String, String> requestHeaders = {
-  //     'Content-Type': "application/json"
-  //   };
-  //
-  //   var url = Uri.https(Config.apiUrl, Config.loginUrl);
-  //
-  //   var response = await client.post(url, headers: , body: jsonEncode(model), )
-  // }
+  static Future<bool> login(LoginModel model) async {
+    Map<String, String> requestHeaders = {'Content-Type': "application/json"};
 
+    var url = Uri.https(Config.apiUrl, Config.loginUrl);
+
+    var response = await client.post(
+      url,
+      headers: requestHeaders,
+      body: jsonEncode(model),
+    );
+
+    if(response.statusCode == 200) {
+
+      return true;
+    } else{
+      return false;
+    }
+  }
 }
+
 
 
 
